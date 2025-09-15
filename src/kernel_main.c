8b18b7f3f62152c321f4838003e3f623f8e40a1a
Rachel Riemersma <rachelriemersma@outlook.com>
2025-09-15 19:44:00 +0000

    idk

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -23,6 +23,10 @@ void main() {
 
     esp_printf(putc, "Current execution level: Kernel Mode\r\n");
     esp_printf(putc, "Terminal driver working!\r\n");	
+    
+    for(int i = 1; i <= 30; i++) {
+	esp_printf(putc, "Line%d - Test scroll\r\n", i);	
+    }
 
     while(1) {
         uint8_t status = inb(0x64);
