79f727de76e1cb751ab4b688a32defc2e7872ece
rachelriemersma <119626666+rachelriemersma@users.noreply.github.com>
2025-10-08 20:04:55 +0000

    removed outb

--- src/keyboard.c
+++ src/keyboard.c
@@ -47,10 +47,6 @@ uint8_t inb(uint16_t port) {
     return rv;
 }
 
-void outb(uint16_t port, uint8_t val) {
-    __asm__ __volatile__ ("outb %0, %1" : : "a" (val), "dN" (port));
-}
-
 void keyboard_handler_main(void) {
     uint8_t scancode;
     
